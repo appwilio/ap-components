be3b8786c842ad968c4b3a6d00a654dd0c38ff09
Evgeniy Baranov <kompolom@yandex.ru>
2016-02-09 08:33:59 +0300

    Update dfp__config.js

--- common.blocks/dfp/__config/dfp__config.js
+++ common.blocks/dfp/__config/dfp__config.js
@@ -1,13 +1,13 @@
 modules.define('dfp__config', function(provide){
     provide({
-        id : 'UA-8075031-4',
-        network: 55222543,
+        id : 'UA-4',
+        network: 0000000000,
         single: true,
         locations : {
             sidebar : {
                 name: 'vrukodelii.sidebar',
                 sizes : [[300, 600], [300, 100], [300, 250]],
-                id : 'div-gpt-ad-1453229100787-0'
+                id : 'div--0'
             },
             footer : {
                 name: 'vrukodelii.4434',
