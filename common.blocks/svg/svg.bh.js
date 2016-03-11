7a59e040722208ff8ce0a344df240968381ea926
Evgeniy Baranov <master@kompolom.ru>
2016-03-11 12:40:52 +0300

    add svg block

---
+++ common.blocks/svg/svg.bh.js
@@ -0,0 +1,9 @@
+module.exports = function(bh) {
+    bh.match('svg', function(ctx) {
+        ctx
+            .tag('svg')
+            .attr('role', 'img')
+            .attr('version', '1.1')
+            .attr('xmlns', 'http://www.w3.org/2000/svg');
+    });
+};
---
+++ common.blocks/svg/svg.tmpl-specs/10-default.bemjson.js
@@ -0,0 +1,8 @@
+({
+    block : 'svg',
+    attrs : {
+        width : 10,
+        height : 10
+    },
+    content : '<path d="M10 .7L9.3 0 5 4.3.7 0 0 .7 4.3 5 0 9.3l.7.7L5 5.7 9.3 10l.7-.7L5.7 5z"/>'
+})
