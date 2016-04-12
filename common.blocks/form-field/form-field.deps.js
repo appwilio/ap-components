3e96e59a78eafc510f646dd8e3bb7880179ae488
Evgeniy Baranov <master@kompolom.ru>
2016-04-12 19:41:30 +0300

    add form-field block

---
+++ common.blocks/form-field/form-field.deps.js
@@ -0,0 +1,14 @@
+([{
+    mustDeps : [
+        { block : 'i-bem', elems : ['dom'] }
+    ]
+}, {
+    tech : 'tmpl-spec.js',
+    mustDeps : [
+        { block : 'message', mods : { type : 'text' } },
+        { block : 'message', mods : { type : 'popup' } },
+        { block : 'form-field', mods : { type : ['hidden'] } },
+        { block : 'form-field', mods : { message : 'popup' } },
+        { block : 'form-field', mods : { message : 'text' } },
+    ]
+}])
