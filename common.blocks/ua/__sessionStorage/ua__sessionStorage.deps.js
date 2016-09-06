a20dd602eb0439a64ef25a020afa69dd9b793035
Evgeniy Baranov <master@kompolom.ru>
2016-09-06 13:47:05 +0300

    add seesionstorage test

---
+++ common.blocks/ua/__sessionStorage/ua__sessionStorage.deps.js
@@ -0,0 +1,4 @@
+({
+    mustDeps: ['ua'],
+    shouldDeps: []
+})
---
+++ common.blocks/ua/__sessionStorage/ua__sessionStorage.js
@@ -0,0 +1,18 @@
+modules.define('ua', function(provide, ua){
+    function checkSessionStorage() {
+        try {
+            var storage = window.sessionStorage,
+                x = '__storage_test__';
+            storage.setItem(x, x);
+            storage.removeItem(x);
+            return true;
+        }
+        catch(e) {
+            return false;
+        }
+    }
+
+    ua.sessionStorage = checkSessionStorage();
+    provide(ua);
+});
+
