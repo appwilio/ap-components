b49ddeeb0f5fdfb522277e1a8a063f1d6d0b1c62
Sergey Belozyorcev <belozyorcev@ya.ru>
2019-08-26 13:58:45 +0300

    Fix attach decl modifer

--- common.blocks/attach/_multiple/attach_multiple.browser.js
+++ common.blocks/attach/_multiple/attach_multiple.browser.js
@@ -4,7 +4,7 @@ modules.define('attach',
                ['i-bem__dom'],
                function(provide, BEMDOM) {
 
-    BEMDOM.decl({ block : 'attach', modName : 'multiple', modVal : true }, {
+    provide(BEMDOM.decl({ block : 'attach', modName : 'multiple', modVal : true }, {
 
         _onChange : function() {
             var control = this.findElem('control');
@@ -19,9 +19,7 @@ modules.define('attach',
         getFiles : function(){
             return this._files;
         }
-    });
-
-    provide(BEMDOM);
+    }));
 
 });
 
